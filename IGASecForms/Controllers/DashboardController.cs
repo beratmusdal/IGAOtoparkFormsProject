c2b5cb5ce27da987d132c6cba457ba2b5d9496a0
beratmusdal <beratmsdl@gmail.clm>
2025-07-16 16:51:32 +0300

    commit

---
+++ IGASecForms/Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IGAOtoParkFormsProject.UI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult _HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _AsidePartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _SubHeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _FooterPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _ScriptsPartial()
+        {
+            return PartialView();
+        }
+    }
+}
